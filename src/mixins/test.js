f4985fc3d73f2ad71d7d178b6f2aa4122102d699
zhuangfeifei <1429156923@qq.com>
2019-05-08 16:26:28 +0800

    创建项目并且完成底部导航

---
+++ src/mixins/test.js
@@ -0,0 +1,18 @@
+import wepy from 'wepy'
+
+export default class testMixin extends wepy.mixin {
+  data = {
+    mixin: 'This is mixin data.'
+  }
+  methods = {
+    
+  }
+
+  onShow() {
+    // console.log('mixin onShow')
+  }
+
+  onLoad() {
+    // console.log('mixin onLoad')
+  }
+}
